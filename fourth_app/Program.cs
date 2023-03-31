651a4154dce518c6145deb8b889fe0e7e3a1c2ef
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-03-31 13:36:20 +0200

    testing some stuff with arrays

---
+++ fourth_app/Program.cs
@@ -0,0 +1,35 @@
+﻿// C# program to illustrate the
+// concept of object array
+using System;
+
+class GFG {
+
+	// Main method
+	static public void Main()
+	{
+
+		// Creating and initializing
+		// object array
+		object[] arr = new object[] { "Test", "Test2", "Test3", "Test4" };
+
+		// arr[0] = 3.899;
+		// arr[1] = 3;
+		// arr[2] = 'g';
+		// arr[3] = "Greeks";
+
+		// it will display
+		// nothing in output
+		// arr[4] = null;
+
+		// it will show System.Object
+		// in output
+		// arr[5] = new object();
+
+		// Display the element of
+		// an object array
+		foreach(var item in arr)
+		{
+			Console.WriteLine(item);
+		}
+	}
+}
