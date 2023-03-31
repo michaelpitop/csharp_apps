b0ee3c869ad2d1448ca075fc13e3002facf83237
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-03-31 11:53:08 +0200

    second app

---
+++ second_app/Program.cs
@@ -0,0 +1,20 @@
+﻿
+class GlazerCalc
+{
+  static void Main()
+  {
+        double width, height, woodLength, glassArea;
+        string widthString, heightString;
+
+        widthString = Console.ReadLine();
+        width = double.Parse(widthString);
+
+        heightString = Console.ReadLine();
+        height = double.Parse(heightString);
+
+        woodLength = 2 * ( width + height ) * 3.25 ;
+        glassArea = 2 * ( width + height ) ;
+        System.Console.WriteLine( "The length of the wood is " + woodLength + "  feet") ;
+        System.Console.WriteLine( "The area of the glass is " + glassArea + "  square metres") ;
+  }
+}
