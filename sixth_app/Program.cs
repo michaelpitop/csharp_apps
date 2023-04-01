c1b6a653f29c131b49f0829cb03e897ba5ca021b
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-04-01 12:17:37 +0200

    app that asks user input for a series of numbers and when they add a negative number it stops and gives you the total sum and average

---
+++ sixth_app/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a series of positive numbers (enter a negative number to exit):");
+
+        int sum = 0;
+        int count = 0;
+
+        while (true)
+
+        {
+          if (int.TryParse(Console.ReadLine(), out int number) && number >= 0)
+          {
+            sum += number;
+            count++;
+          }
+          else
+          {
+            break;
+          }
+        }
+        if (count > 0)
+        {
+          double average = (double) sum / count;
+          Console.WriteLine($"The sum is {sum} and the average is {average:N2}");
+        }
+        else
+        {
+          Console.WriteLine("No positive numbers were entered.");
+        }
+          Console.ReadLine();
+    }
+}
