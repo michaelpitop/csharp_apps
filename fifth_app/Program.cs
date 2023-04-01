60cb255e8760109298fab19b0d05254ef448abf2
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-04-01 11:38:18 +0200

    made a word count app that takes user input

---
+++ fifth_app/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WordCountApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence or a paragraph:");
+            string userInput = Console.ReadLine();
+
+            // Split the input string into an array of words
+            string[] words = userInput.Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Count the number of words
+            int wordCount = words.Length;
+
+            // Display the word count
+            Console.WriteLine("The number of words in the input is: " + wordCount);
+
+            Console.ReadLine();
+        }
+    }
+}
