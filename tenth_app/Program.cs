0a37c6fe204d1d921ff9b8600fbece4c24d12ca2
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-04-02 11:47:53 +0200

    simple to do list app

---
+++ tenth_app/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> toDoList = new List<string>();
+
+            while (true)
+            {
+                Console.WriteLine("Enter a task to add to your to-do list, or type 'quit' to exit:");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "quit")
+                {
+                    break;
+                }
+                else
+                {
+                    toDoList.Add(input);
+                    Console.WriteLine("Task added to to-do list.");
+                }
+            }
+
+            Console.WriteLine("\nYour to-do list:");
+            foreach (string task in toDoList)
+            {
+                Console.WriteLine(task);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
