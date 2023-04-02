2485ec60bdb50acb207e3e4a7d313c451060c04d
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-04-02 11:36:37 +0200

    simple calculator

---
+++ eighth_app/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Simple Calculator");
+
+        Console.Write("Enter the first number: ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Select an operation (+ - * /): ");
+        char op = char.Parse(Console.ReadLine());
+
+        double result = 0;
+
+        switch (op)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Invalid operator selected.");
+                break;
+        }
+
+        Console.WriteLine($"Result: {num1} {op} {num2} = {result}");
+        Console.ReadLine();
+    }
+}
