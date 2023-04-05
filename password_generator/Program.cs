278b3ec4fbd3d0a1faf6482be2f878fa369d5eb3
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-04-05 12:48:10 +0200

    wrote this app yesterday on my phone (lol) and it works decently well, probably can improve it more in the future

---
+++ password_generator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Generated password: " + GeneratePassword());
+    }
+
+    static string GeneratePassword() {
+        const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=[]{};:,.<>?";
+        StringBuilder password = new StringBuilder();
+        Random rnd = new Random();
+        for (int i = 0; i < 20; i++) {
+            password.Append(validChars[rnd.Next(validChars.Length)]);
+        }
+        return password.ToString();
+    }
+}
