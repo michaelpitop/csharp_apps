e606b5715178b53305f8f880dd9f004484c28749
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-04-06 13:08:39 +0200

    started with the app and wrote some comments about where to continue

---
+++ rpg_game_2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// user chooses their name
+// user gets to choose one of the selected races
+// each race has different attributes
+
+// we add multiple enemies like last time (not public)
+// add a multiplier with random effect and whenever its above *2 it gives a  message about critical hit
+
+using System;
+
+namespace RPGGame2
+{
+  class Programm
+  {
+    static void Main(string[] args)
+    {
+      public class Player {
+        private string _name
+        public String Stats {
+          get {
+
+          }
+        }
+      }
+    }
+
+  }
+}
