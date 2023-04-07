76405c28ae43b42942e7f84f93a7c0ad3eca0107
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-04-07 12:51:42 +0200

    a lot of problems... Cant figure out how to give the user a choice of race in numbers and then turn it into the races name in console

--- rpg_game_2/Program.cs
+++ rpg_game_2/Program.cs
@@ -1,5 +1,4 @@
-﻿
-// user chooses their name
+﻿// user chooses their name
 // user gets to choose one of the selected races
 // each race has different attributes
 
@@ -7,47 +6,68 @@
 // add a multiplier with random effect and whenever its above *2 it gives a  message about critical hit
 
 using System;
+using System.Collections.Generic;
 
 namespace RPGGame2
 {
-  public class Programm
-  {
-    static void Main(string[] args)
+    public class Program
     {
-    PlayerInput playerInput = new PlayerInput();
+        static void Main(string[] args)
+        {
+            PlayerInput playerInput = new PlayerInput();
 
-    Console.WriteLine("Please enter your name: ");
+            Console.WriteLine("Please enter your name: ");
+            playerInput.Name = Console.ReadLine();
 
-    playerInput.Name = Console.ReadLine();
+            List<string> raceOptions = new List<string>() { "Human", "Elf", "Dwarf", "Orc", "Goblin" };
 
-    Console.WriteLine("Please choose a race: ");
+            Console.WriteLine("Please choose a race: ");
+            for (int i = 0; i < raceOptions.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {raceOptions[i]}");
+            }
 
-    playerInput.Race = int.Parse(Console.ReadLine());
+            int raceChoice;
+            while (!int.TryParse(Console.ReadLine(), out raceChoice) || raceChoice < 1 || raceChoice > raceOptions.Count)
+            {
+                Console.WriteLine($"Invalid input. Please choose a number between 1 and {raceOptions.Count}");
+            }
 
-    int _race;
-
-    string _name;
-
-    Console.WriteLine("Your name is " + $"{playerInput.Name}");
-    Console.WriteLine($"{playerInput.Race}");
-
-  }
-
-  public class PlayerInput {
-    private string _name;
-    private int _race;
-
-    public string Name  {
-      get { return _name;}
-      set { _name = value; }
-      }
-
-    public int Race {
-      get { return _race; }
-      set { _race = value; }
-      }
+            Console.WriteLine($"Your name is {playerInput.Name}");
+            Console.WriteLine($"Your race is {playerInput.Race}");
+        }
     }
 
-    }
+    public class PlayerInput
+    {
+        private string _name = "";
+        private int _race;
 
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Race
+            {
+                get { return _race; }
+                set { _race = value; }
+            }
+
+            public List<string> RaceOptions
+            {
+                get
+                {
+                    return new List<string> {
+                    "Human",
+                    "Elf",
+                    "Dwarf",
+                    "Orc",
+                    "Goblin"
+                };
+                }
+            }
+
+    }
 }
