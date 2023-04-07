b34a680362071cf815b2a76e96151406d62ff030
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-04-07 14:22:04 +0200

    UGH.... I made this app to better understand how to turn an int user input into a string of prechosen races but im having multiple problems when the user chooses an invalid choice. Either the loop wont end so its constantly printing Invalid Choice OR when you get an invalid choice i wrote it so you can choose again BUT if you put a valid choice after an invalid choice YOU STILL GET INVALID?! yeah will have to figure this one out an other time

---
+++ IntToStringConversion/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Please choose a race:  \n1. Human \n2. Elf \n3. Dwarf \n4. Orc \n5. Goblin");
+
+string raceString = "";
+int raceChoice = int.Parse(Console.ReadLine());
+
+bool validInput = false;
+while (!validInput)
+{
+    if (int.TryParse(Console.ReadLine(), out raceChoice) && raceChoice >= 1 && raceChoice >= 5)
+    {
+        validInput = true;
+        switch (raceChoice)
+        {
+            case 1:
+                raceString = "Human";
+                break;
+            case 2:
+                raceString = "Elf";
+                break;
+            case 3:
+                raceString = "Dwarf";
+                break;
+            case 4:
+                raceString = "Orc";
+                break;
+            case 5:
+                raceString = "Goblin";
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid input, please try again.");
+    }
+}
+
+
+Console.WriteLine($"Your chosen race is {raceString}");
