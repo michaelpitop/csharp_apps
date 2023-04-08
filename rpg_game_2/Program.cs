0974e1a39cb8c229d6b80f44c335374da478af76
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-04-08 13:09:10 +0200

    stupid commit, nothing of importance

--- rpg_game_2/Program.cs
+++ rpg_game_2/Program.cs
@@ -1,7 +1,7 @@
-﻿// --user chooses their name--
+﻿// ~~user chooses their name~~
 // ~~user gets to choose one of the selected races~~
-// each race has different attributes
 
+// each race has different attributes
 // we add multiple enemies like last time (not public)
 // add a multiplier with random effect and whenever its above *2 it gives a  message about critical hit
 
