71a5a1f6f8f683e23618ed7b6ad0f5699f6b8faf
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-04-08 11:54:59 +0200

    after yesterdays meltdown i finally made it work... All because o f my stupid ass putting ReadLine TWICE! no wonder that it required user input twice and the second input was not societed with the rest of the app.. Anyway, it works.. 🙄

--- IntToStringConversion/Program.cs
+++ IntToStringConversion/Program.cs
@@ -1,38 +1,48 @@
 ﻿Console.WriteLine("Please choose a race:  \n1. Human \n2. Elf \n3. Dwarf \n4. Orc \n5. Goblin");
 
 string raceString = "";
-int raceChoice = int.Parse(Console.ReadLine());
+int raceChoice = 0;
 
 bool validInput = false;
 while (!validInput)
 {
-    if (int.TryParse(Console.ReadLine(), out raceChoice) && raceChoice >= 1 && raceChoice >= 5)
+    if (int.TryParse(Console.ReadLine(), out raceChoice) && raceChoice >= 1 && raceChoice <= 5)
     {
         validInput = true;
         switch (raceChoice)
         {
             case 1:
                 raceString = "Human";
+                Console.WriteLine($"Your chosen race is {raceString}");
                 break;
             case 2:
                 raceString = "Elf";
+                Console.WriteLine($"Your chosen race is {raceString}");
                 break;
             case 3:
                 raceString = "Dwarf";
+                Console.WriteLine($"Your chosen race is {raceString}");
                 break;
             case 4:
                 raceString = "Orc";
+                Console.WriteLine($"Your chosen race is {raceString}");
                 break;
             case 5:
                 raceString = "Goblin";
+                Console.WriteLine($"Your chosen race is {raceString}");
                 break;
+            default:
+              raceString = "Invalid";
+              Console.WriteLine($"Your chosen race is {raceString}");
+              break;
         }
     }
     else
     {
         Console.WriteLine("Invalid input, please try again.");
+        break;
     }
 }
 
 
-Console.WriteLine($"Your chosen race is {raceString}");
+// Console.WriteLine($"Your chosen race is {raceString}");
