7b9cc22f7f71fe389775ef001ff2b02d927527fb
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-04-12 13:30:15 +0200

    Created a program that reads data from a JSON file and displays it in a console window.

---
+++ JSON_reader/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main()
+    {
+        // Read the JSON file
+        string json = File.ReadAllText("books.json");
+
+        // Deserialize the JSON into a dynamic object
+        dynamic data = JsonConvert.DeserializeObject(json);
+
+        // Display the books in the console
+        foreach (var book in data.books)
+        {
+            Console.WriteLine("Title: " + book.title);
+            Console.WriteLine("Author: " + book.author);
+            Console.WriteLine("Year: " + book.year);
+            Console.WriteLine();
+        }
+
+        // Wait for the user to press a key before closing the console
+        Console.ReadKey();
+    }
+}
