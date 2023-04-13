bd2952d471555357cae70cb8db234da1f4e1b741
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-04-13 13:34:24 +0200

    wrote an app that takes user inputed numbers and gives them back in descending order using the Split(),Select(), Where(), OrderByDescending() and toArray() methods

---
+++ LinqChallenge/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqChallenge
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Enter a list of numbers (separated by commas): ");
+      string input = Console.ReadLine()  ?? "";
+
+      int[] numbers = input.Split(',')
+                           .Select(x => int.Parse(x.Trim()))
+                           .Where(x => x <= 1000)
+                           .OrderByDescending(x => x)
+                           .ToArray();
+      Console.WriteLine($"Filtered list (in descending order): {string.Join(", ", numbers)}");
+    }
+  }
+}
