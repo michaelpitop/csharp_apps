ef549d8624cd9daba7f23a0c1e9d912ff4e09752
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-04-15 13:04:41 +0200

    Nothing special just refreshing my memory in Regex. Recreated an old regex app i wrote in ruby for C#

---
+++ RegularExpressions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your email address: ");
+        string email = Console.ReadLine() ?? string.Empty;
+
+        // Define a regular expression pattern for validating email addresses
+        string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+        // Create a regular expression object with the pattern
+        Regex regex = new Regex(pattern);
+
+        // Use the regular expression object to validate the input
+        if (regex.IsMatch(email))
+        {
+            Console.WriteLine("Valid email address.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid email address.");
+        }
+
+        Console.ReadKey();
+    }
+}
