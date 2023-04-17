ab476685a37aa27d3ffe7661839d3a188c51a15e
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-04-17 21:04:05 +0200

    learning about partial classes

---
+++ PartialClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PartialClasses
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      Development development = new Development();
+      development.Backend();
+      development.Frontend();
+      development.Database();
+    }
+  }
+
+  public partial class Development
+  {
+    public void Frontend() => Console.WriteLine("This is the frontend part of development");
+  }
+
+  public partial class Development
+  {
+    public void Backend() => Console.WriteLine("This is the backend part of development");
+  }
+
+  public partial class Development
+  {
+    public void  Database() => Console.WriteLine("This is when the database part of development.");
+  }
+}
