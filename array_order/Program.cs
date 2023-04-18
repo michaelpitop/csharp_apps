bd45b678e0d0b65906fc4b68878b838333d9258f
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-04-18 14:56:14 +0200

    just rewriting an exercise from the Microsoft page to learn more about arrays and foreach

---
+++ array_order/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+  public static void Main()
+  {
+    string[] orderIDs = { "B123", "B124", "C123", "C546", "A890" };
+
+    foreach (string orderID in orderIDs)
+    {
+      if (orderID.StartsWith("A"))
+      {
+        Console.WriteLine(orderID);
+      }
+    }
+  }
+}
