719e941ccd3f67f7f2e74e126ef85e5bcf9fe756
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-04-20 13:58:36 +0200

    added stats for each race, still need to fix a minor warning error next time

--- rpg_game_2/Program.cs
+++ rpg_game_2/Program.cs
@@ -1,8 +1,10 @@
 ﻿// ~~user chooses their name~~
 // ~~user gets to choose one of the selected races~~
 
-// each race has different attributes
-  // 1. we need to add
+// ~~each race has different attributes~~
+  // ~~1. we need to add all races in an array~~
+  // ~~2. all objects in the array needs to have specific stats~~
+  
 // we add multiple enemies like last time (not public)
 
 
@@ -24,51 +26,70 @@ namespace RPGGame2
 
             Console.WriteLine("Please choose a race:  \n1. Human \n2. Elf \n3. Dwarf \n4. Orc \n5. Goblin");
 
+            Race[] races = new Race[] {
+              new Race("Human", 10, 10, 5, 5),
+              new Race("Elf", 5, 15, 15, 5),
+              new Race("Dwarf", 15, 5, 10, 10),
+              new Race("Orc", 15, 5, 5, 15),
+              new Race("Goblin", 5, 15, 5, 15)
+            };
+
 
             int raceChoice = 0;
-            string raceString = "";
+            // string raceString = "";
 
             bool validInput = false;
             while (!validInput)
             {
-                string input = Console.ReadLine() ?? string.Empty;
-                if (int.TryParse(input, out raceChoice) && raceChoice >= 1 && raceChoice <= 5)
+                if (int.TryParse(Console.ReadLine(), out raceChoice) && raceChoice >= 1 && raceChoice <= 5)
                 {
                     validInput = true;
-                    switch (raceChoice)
-                    {
-                        case 1:
-                            raceString = "Human";
-                            Console.WriteLine($"Your name is {playerInput.Name} and your chosen race is {raceString}");
-                            break;
-                        case 2:
-                            raceString = "Elf";
-                            Console.WriteLine($"Your name is {playerInput.Name} and your chosen race is {raceString}");
-                            break;
-                        case 3:
-                            raceString = "Dwarf";
-                            Console.WriteLine($"Your name is {playerInput.Name} and your chosen race is {raceString}");
-                            break;
-                        case 4:
-                            raceString = "Orc";
-                            Console.WriteLine($"Your name is {playerInput.Name} and your chosen race is {raceString}");
-                            break;
-                        case 5:
-                            raceString = "Goblin";
-                            Console.WriteLine($"Your name is {playerInput.Name} and your chosen race is {raceString}");
-                            break;
-                        default:
-                          raceString = "Invalid";
-                          Console.WriteLine($"Your name is {playerInput.Name} and your chosen race is {raceString}");
-                          break;
-                    }
+                    playerInput.Race = races[raceChoice - 1];
+                    Console.WriteLine($"Your name is {playerInput.Name}. Your chosen race is {playerInput.Race.Name}. Stats: Strength: {playerInput.Race.Strength}, Dexterity: {playerInput.Race.Dexterity}, Intelligence: {playerInput.Race.Intelligence}, Cunning: {playerInput.Race.Cunning}");
                 }
                 else
                 {
                     Console.WriteLine("Invalid input, please try again.");
-                    break;
                 }
-                playerInput.Race = raceChoice;
+
+                // string input = Console.ReadLine() ?? string.Empty;
+                // if (int.TryParse(input, out raceChoice) && raceChoice >= 1 && raceChoice <= 5)
+                // {
+                //     validInput = true;
+                //     switch (raceChoice)
+                //     {
+                //         case 1:
+                //             raceString = "Human";
+                //             Console.WriteLine($"Your name is {playerInput.Name} and your chosen race is {raceString}");
+                //             break;
+                //         case 2:
+                //             raceString = "Elf";
+                //             Console.WriteLine($"Your name is {playerInput.Name} and your chosen race is {raceString}");
+                //             break;
+                //         case 3:
+                //             raceString = "Dwarf";
+                //             Console.WriteLine($"Your name is {playerInput.Name} and your chosen race is {raceString}");
+                //             break;
+                //         case 4:
+                //             raceString = "Orc";
+                //             Console.WriteLine($"Your name is {playerInput.Name} and your chosen race is {raceString}");
+                //             break;
+                //         case 5:
+                //             raceString = "Goblin";
+                //             Console.WriteLine($"Your name is {playerInput.Name} and your chosen race is {raceString}");
+                //             break;
+                //         default:
+                //           raceString = "Invalid";
+                //           Console.WriteLine($"Your name is {playerInput.Name} and your chosen race is {raceString}");
+                //           break;
+                //     }
+                // }
+                // else
+                // {
+                //     Console.WriteLine("Invalid input, please try again.");
+                //     break;
+                // }
+                // playerInput.Race = raceChoice;
             }
 
         }
@@ -77,7 +98,7 @@ namespace RPGGame2
     public class PlayerInput
     {
         private string _name = "";
-        private int _race;
+        private Race _race;
 
         public string Name
         {
@@ -85,12 +106,54 @@ namespace RPGGame2
             set { _name = value; }
         }
 
-        public int Race
+        public Race Race
         {
           get { return _race; }
           set { _race = value; }
         }
+    }
 
+    public class Race
+    {
+      private string _name = "";
+      private int _strength;
+      private int _dexterity;
+      private int _intelligence;
+      private int _cunning;
+
+      public Race(string name, int strength, int dexterity, int intelligence, int cunning)
+      {
+        _name = name;
+        _strength = strength;
+        _dexterity = dexterity;
+        _intelligence = intelligence;
+        _cunning = cunning;
+      }
+
+      public string Name
+      {
+        get { return _name; }
+      }
+
+      public int Strength
+      {
+        get { return _strength; }
+      }
+
+      public int Dexterity
+      {
+        get { return _dexterity; }
+      }
+
+      public int Intelligence
+      {
+        get { return _intelligence; }
+      }
+
+      public int Cunning
+      {
+        get { return _cunning; }
+      }
 
     }
 }
