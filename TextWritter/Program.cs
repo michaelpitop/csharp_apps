d7f8fa9935e9ff06e99dddd108771d0ca9a1d8b1
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-04-26 16:46:06 +0200

    just learning about StreamWriter and File.CreateText methods

---
+++ TextWritter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+class Program {
+    static void Main(string[] args) {
+        // Ask user for filename and text to write
+        Console.Write("Enter filename: ");
+        string filename = Console.ReadLine() ?? string.Empty;
+        Console.Write("Enter text: ");
+        string text = Console.ReadLine() ?? string.Empty;
+
+        // Create new file and write text to it
+        using (StreamWriter sw = File.CreateText(filename)) {
+            sw.Write(text);
+        }
+
+        Console.WriteLine($"Text written to file '{filename}'");
+    }
+}
---
+++ TextWritter/Test3.rb
@@ -0,0 +1,1 @@
+#This is an unnecessary test but its kinda fun tbh :)
